6964a75a0471cf3faff4ddade5c637fbc6cdd661
Matin shahmaleki <matin.shahmaleki001@gmail.com>
2026-02-19 15:09:22 +0330

    Restrict document dropdowns to control hierarchy and disable create/open

--- approvals_documents_override/models/approvals_category.py
+++ approvals_documents_override/models/approvals_category.py
@@ -4,7 +4,6 @@ from odoo import api, fields, models
 class ApprovalCategory(models.Model):
     _inherit = "approval.category"
 
-    # In this Odoo 19 stack, folder-like hierarchy is stored on documents.document
     document_type_folder_id = fields.Many2one(
         "documents.document",
         string="نوع مدرک",
@@ -34,21 +33,29 @@ class ApprovalCategory(models.Model):
     @api.model
     def _documents_parent_field(self):
         folder_model = self._documents_folder_model()
-        if "parent_folder_id" in folder_model._fields:
-            return "parent_folder_id"
+        for field_name in ("parent_folder_id", "parent_id", "folder_id"):
+            if field_name in folder_model._fields:
+                return field_name
         return "parent_id"
 
     @api.model
     def _documents_folder_domain(self):
         folder_model = self._documents_folder_model()
-        # Keep compatibility with variants where folder records are documents with type='folder'
         if "type" in folder_model._fields:
             return [("type", "=", "folder")]
+        if "is_folder" in folder_model._fields:
+            return [("is_folder", "=", True)]
         return []
 
     @api.model
+    def _children_of(self, parent_record):
+        parent_field = self._documents_parent_field()
+        return self._documents_folder_model().search(
+            self._documents_folder_domain() + [(parent_field, "=", parent_record.id)]
+        )
+
+    @api.model
     def _get_document_control_root(self):
-        """Return the root folder used to feed approval category drop-downs."""
         return self._documents_folder_model().search(
             self._documents_folder_domain() + [("name", "=", "کنترول مستندات")],
             limit=1,
@@ -57,30 +64,20 @@ class ApprovalCategory(models.Model):
     @api.depends("document_type_folder_id")
     def _compute_document_folder_domains(self):
         folder_obj = self._documents_folder_model()
-        parent_field = self._documents_parent_field()
-        folder_domain = self._documents_folder_domain()
         root_folder = self._get_document_control_root()
 
         for rec in self:
-            if root_folder:
-                rec.document_type_allowed_ids = folder_obj.search(
-                    folder_domain + [(parent_field, "=", root_folder.id)]
-                )
-            else:
-                rec.document_type_allowed_ids = folder_obj.browse()
-
-            if rec.document_type_folder_id:
-                rec.document_owner_allowed_ids = folder_obj.search(
-                    folder_domain + [(parent_field, "=", rec.document_type_folder_id.id)]
-                )
-            else:
-                rec.document_owner_allowed_ids = folder_obj.browse()
+            rec.document_type_allowed_ids = (
+                self._children_of(root_folder) if root_folder else folder_obj.browse()
+            )
+            rec.document_owner_allowed_ids = (
+                self._children_of(rec.document_type_folder_id)
+                if rec.document_type_folder_id
+                else folder_obj.browse()
+            )
 
     @api.onchange("document_type_folder_id")
     def _onchange_document_type_folder_id(self):
-        parent_field = self._documents_parent_field()
-        folder_domain = self._documents_folder_domain()
-
         self.document_owner_folder_id = False
         if not self.document_type_folder_id:
             return {
@@ -88,10 +85,10 @@ class ApprovalCategory(models.Model):
                     "document_owner_folder_id": [("id", "=", False)],
                 }
             }
-
         return {
             "domain": {
-                "document_owner_folder_id": folder_domain
-                + [(parent_field, "=", self.document_type_folder_id.id)],
+                "document_owner_folder_id": [
+                    ("id", "in", self._children_of(self.document_type_folder_id).ids)
+                ],
             }
         }
