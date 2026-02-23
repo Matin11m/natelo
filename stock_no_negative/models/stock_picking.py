c39e0bf94afcdbd503daa5fc52e516127560d948
Matin shahmaleki <matin.shahmaleki001@gmail.com>
2026-02-23 13:05:36 +0330

    [FIX] stock_no_negative: lock date_done after validation

--- stock_no_negative/models/stock_picking.py
+++ stock_no_negative/models/stock_picking.py
@@ -2,6 +2,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from odoo import fields, models
+from odoo.exceptions import UserError
 
 
 class StockPicking(models.Model):
@@ -19,5 +20,14 @@ class StockPicking(models.Model):
         res = super()._action_done()
         for picking in self:
             if saved_dates.get(picking.id):
-                picking.date_done = saved_dates[picking.id]
+                picking.with_context(allow_done_date_write=True).date_done = saved_dates[picking.id]
         return res
+
+    def write(self, vals):
+        if "date_done" in vals and not self.env.context.get("allow_done_date_write"):
+            done_pickings = self.filtered(lambda p: p.state == "done")
+            if done_pickings:
+                raise UserError(
+                    self.env._("You cannot modify Done Date after the picking is validated.")
+                )
+        return super().write(vals)
